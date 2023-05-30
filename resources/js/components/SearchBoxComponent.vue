8cbd38f5c7078a9d1a60a61bceae5eb8585de8bc
Nae <demon.nae@gmail.com>
2023-05-30 23:18:07 +0700

    add searchbox component

---
+++ resources/js/components/SearchBoxComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center mt-3">
+            <div class="col-lg-6">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search" v-model="searchText">
+                    <button class="btn btn-outline-secondary" type="button" @click="search">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            searchText: '',
+        };
+    },
+    methods: {
+        search() {
+            // Add your search logic here
+            console.log(this.searchText);
+        },
+    },
+};
+</script>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Zero2One Assignment</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- Styles -->
+    <style>
+
+    </style>
+</head>
+
+<body id="app">
+    <search-box-component></search-box-component>
+</body>
+</html>
